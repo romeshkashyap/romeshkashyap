4a3d25296471a8a6564f4f6514ba143dd5b4ce71
romeshkashyap <126394379+romeshkashyap@users.noreply.github.com>
2023-07-24 21:08:37 +0530

    Add files via upload

---
+++ ex34.py
@@ -0,0 +1,34 @@
+animals = ['bear' ,'python3 . 6','peacock' , 'kangaroo' ,'whale' ,'platypus']
+print(animals[1])#1. The animal at 1.
+print(animals[2])#2. The third (3rd) animal.
+print(animals[0])#3. The first (1st) animal.
+print(animals[3])#4. The animal at 3.
+print(animals[4])#5. The fifth (5th) animal.
+print(animals[2])#6. The animal at 2.
+print(animals[5])#7. The sixth (6th) animal.
+print(animals[4])#8. The animal at 4.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Ordinal number
+#cardinal number
+#Remember: ordinal == ordered, 1st; cardinal == cards at random, 0.
+
